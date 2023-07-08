ab759acda1f37bc6554fb4978ceb8afb415aafaf
Ben <30612894+BenHavis@users.noreply.github.com>
2023-07-08 10:54:11 -0400

    Add files via upload

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React from 'react'
+import PropTypes from 'prop-types'
 
 const Card = ({ header, body }) => {
   return (
-    <div className="w-64 bg-white rounded-lg shadow-md mx-4 p-4">
-      <h2 className="text-xl font-semibold mb-2">{header}</h2>
-      <p className="text-gray-700">{body}</p>
+    <div className='w-64 bg-white rounded-lg shadow-md mx-4 p-4'>
+      <h2 className='text-xl font-semibold mb-2'>{header}</h2>
+      <p className='text-gray-700'>{body}</p>
     </div>
-  );
+  )
 };
 
-export default Card;
+Card.propTypes = {
+  header: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired
+}
+
+export default Card

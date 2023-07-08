e5b57291b5d4e63fd191a4d5db229fd5855a4cb3
Ben <30612894+BenHavis@users.noreply.github.com>
2023-07-07 22:34:15 -0400

    Add files via upload

---
+++ api/config.js
@@ -0,0 +1,7 @@
+// export const GOOGLE_MAPS_API_KEY = 'AIzaSyBsY7TWsifdD_oYfwhEKBqVdLzfBaSeu6A'
+
+module.exports = {
+  GOOGLE_MAPS_API_KEY: 'AIzaSyBsY7TWsifdD_oYfwhEKBqVdLzfBaSeu6A',
+  SUPABASE_URL: 'https://sxjdyfdpdhepsgzhzhak.supabase.co',
+  SUPABASE_API_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4amR5ZmRwZGhlcHNnemh6aGFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg3NTAxNjYsImV4cCI6MjAwNDMyNjE2Nn0.EX7h2N3pr-Lg1dAY2tlMe6kyfH94Q941ioSj2bE5WYU'
+}
